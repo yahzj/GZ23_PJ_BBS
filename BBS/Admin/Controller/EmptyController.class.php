b395284a76be411772deb5c83529076b5e7f84bd
dahuang <332685057@qq.com>
2017-01-05 21:02:14 +0800

    EmptyController

---
+++ BBS/Admin/Controller/EmptyController.class.php
@@ -0,0 +1,17 @@
+<?php 
+namespace Admin\Controller;
+use Think\Controller;
+
+class EmptyController extends Controller{
+
+	// 8.空操作(找不同请求的操作时)
+	public function _empty($var){
+		dump($var);
+	}
+
+	// EmptyController
+
+
+}
+
+
