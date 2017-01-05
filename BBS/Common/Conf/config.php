9b9ddbc2411b39bf185af2f28dc875881981b05c
dahuang <332685057@qq.com>
2017-01-05 20:37:24 +0800

    config.php修改

--- BBS/Common/Conf/config.php
+++ BBS/Common/Conf/config.php
@@ -11,13 +11,13 @@ return array(
 	'DEFAULT_MODULE'       =>    'Home',  
 
 	 /* 数据库设置 */
-    'DB_TYPE'               =>  '',     // 数据库类型
-    'DB_HOST'               =>  '', // 服务器地址
-    'DB_NAME'               =>  '',          // 数据库名
-    'DB_USER'               =>  '',      // 用户名
+    'DB_TYPE'               =>  'mysqli',     // 数据库类型
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'my_bbs',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
-    'DB_PORT'               =>  '',        // 端口
-    'DB_PREFIX'             =>  '',    // 数据库表前缀
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  'mybbs_',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
     'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
