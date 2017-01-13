f91c19c506c6c01f8779c7ce5d0730a0b5acecb9
Zhu Guanrong <351251550@qq.com>
2017-01-13 21:00:25 +0800

    登录

--- BBS/Admin/Controller/LoginController.class.php
+++ BBS/Admin/Controller/LoginController.class.php
@@ -2,23 +2,33 @@
 namespace Admin\Controller;
 use Think\Controller;
 class LoginController extends Controller {
-    
+                //登录界面
 	   public function login(){
 
 	         $this->display();
 	   }
-
+               //编辑界面
 	   public function dologin(){
+
+                $verify=I('param.verify','');
+                if(!check_verify($verify)){
+                	$this->error("亲，验证码输错了哦！",$this->site_url,9);
+                }
+  
 	   	$post_admin_name=I('post.admin_name');
 	   	$post_pass=I('post.pass');
                          $map['admin_name']=$post_admin_name;
                          $admin=D('admin');
                          $list=$admin->where($map)->select(); 
-                         $res=password_verify($post_pass,$list['pass']);
+                         //dump($list);
+                         if ($list[0]['admin_name']==null){
+                                 return $this->Error('帐号不存在');
+                         }
+                         $res=password_verify($post_pass,$list[0]['pass']);
                          if($res){
-                         	      return $admin->success("登录成功！");
+                         	      return $this->success("登录成功！",U('Admin/Admin/index'));
                          }else{
-                                return $admin->getError();//验证错误，返回错误信息
+                                return $this->Error('密码不正确');//验证错误，返回错误信息
                          }
                                             
 	   }
@@ -29,13 +39,16 @@ class LoginController extends Controller {
 		    // 实例化Verify对象
 		    $verify = new \Think\Verify();
 		    // 配置验证码参数
-		    // $verify->fontSize = 14;     // 验证码字体大小
+		    $verify->expire = 9999990;     // 验证码字体大小
 		    // $verify->length = 4;        // 验证码位数
 		    // $verify->imageH = 34;       // 验证码高度
 		    // $verify->useImgBg = true;   // 开启验证码背景
 		    // $verify->useNoise = false;  // 关闭验证码干扰杂点
 		     $verify->entry();
 		}
+
+             
+
  
 
 }
--- BBS/Common/Common/function.php
+++ BBS/Common/Common/function.php
@@ -1,69 +1,12 @@
 <?php
 //配合自动完成加密函数
-// $val为需要哈希加密的数据
-// return返回加密后的数据
 function myHash($val){
 	$hash=password_hash($val,PASSWORD_DEFAULT);
 	return $hash;
 }
 
-
-//Memcache数据库公共缓存
-// 用于减少数据库的读取次数
-// $table 为表单名称
-// $data为数据在Memcache里的键值
-function MemcacheModel($table,$data){
-	// 1.实例化对象
-	$mem = new Memcache();
-	// 2.连接服务器
-	$mem->connect('127.0.0.1',11211);
-
-	$list=[];
-	$list = $mem->get($data);
-
-	if(!$list){
-
-		$obj=D($table);
-		
-		$list=$obj->pro_index();
-		
-		// 写入缓存
-		$mem->set($data,$list,MEMCACHE_COMPRESSED,10);
-		return $list;
-	}
-	return $list;
-}
-
-
-//page页数生成
-// $mod 如果在mod类里调用一般传入$this即可，在控制器里调用，需要传入实例化的mod类
-// $map 传入需要查询的条件，默认没有条件
-// $key 传入排序的属性基准,默认排序以id排序
-// $order 传入排序方式，默认为asc
-function PageMod($mod,$map=[],$key='id',$order=''){
-	// 计算总行数
-	$totalRow = $mod->where($map)->count();
-	// 定义每页显示行数
-	$rows = 10;
-		//判断页数
-	if ($_GET['p']>ceil($totalRow/$rows)) {
-		$_GET['p']=ceil($totalRow/$rows);
-	};
-	// 实例化分页类
-	$page = new \Think\Page( $totalRow,$rows );
-	// 执行查询
-	$list = $mod->where($map)->order('`'.$key.'` '.$order)->limit( $page->firstRow . ',' . $page->listRows   )->select();
-
-	$status = ['锁定','正常','高亮'];
-	// 基本处理
-	foreach($list as $key => &$val){
-		$val['status'] = $status[ $val['status'] ];
-		$val['content'] = substr($val['content'],0,128);
-	}
-
-	return [
-			// 返回用户信息
-			'list' => $list,
-			'show' => $page->show(),
-		];
-}
+//验证码检查
+ function check_verify($code,$id=''){
+          	$verify = new \Think\Verify();   
+          	 return $verify->check($code, $id);
+   }
