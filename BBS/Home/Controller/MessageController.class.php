9e6226e3e56ea725f339c82fba8f785fd6da2d9f
ming <ming@163.com>
2017-01-17 20:01:38 +0800

    前台message差通知和好友请求

--- BBS/Home/Controller/MessageController.class.php
+++ BBS/Home/Controller/MessageController.class.php
@@ -5,18 +5,23 @@ class MessageController extends EmptyController{
 	//显示所有数据和搜索后的数据
 	public function index(){
 		$message = D("message");
+		//什么参数都没有，默认就是收件箱。
 		if(empty(I('get.'))){
 			$list=$message->sms();
 		}else{
+			//从参数的不同跳转到不同的地方。
 			switch(I('get.type')){
 			case 'sms'://短消息
 				$list=$message->sms();
 				break;
 			case 'notice'://系统通知，评论通知
-				$message->notice();
+				$message->sms();
 				break;
 			case 'request'://好友请求
-				$message->request();
+				$message->sms();
+				
+				break;
+			case 'post'://发新消息
 				
 				break;
 			default:
@@ -41,12 +46,12 @@ class MessageController extends EmptyController{
 		}
 		
 	}
-
+	//处理已读
 	public function tag(){
 		$message=D('message');
 		$message->pro_tag();
 	}
-
+	//短消息回复
 	public function add(){
 
 		$message=D('message');
@@ -56,6 +61,50 @@ class MessageController extends EmptyController{
 		}else{
 			$this->error("发送失败");
 		}
+	}
+
+	public function post(){
+		//收信人不能为空
+		if(empty(I('post.receivername'))){
+       		$this->error("收信人不能为空！");
+       	}
+       	//标题不能为空
+		if(empty(I('post.title'))){
+       		$this->error("标题不能为空！");
+       	}
+       	//内容不能为空
+		if(empty(I('post.content'))){
+       		$this->error("内容不能为空！");
+       	}
+		//验证码验证
+        $verify=I('param.verify','');
+        if(!check_verify($verify)){
+       	 	$this->error("亲，验证码输错了哦！");
+       	 }
+       	 $message=D('message');
+       	 $data=$message->pro_post();
+       	 if($data===0){
+       	 	$this->error("没有这个用户",'index/type/post');
+       	 	
+       	 }elseif($data===1){
+       	 	$this->success("发送成功",'index/type/sms/action/send');
+       	 }else{
+       	 	$this->error("系统出错，请稍后再试！！",'index/type/post');
+       	 }
+	}
+
+   //验证码
+    public function verify(){
+		ob_clean();
+	    // 实例化Verify对象
+	    $verify = new \Think\Verify();
+	    // 配置验证码参数
+	    $verify->expire = 9999990;     // 验证码字体大小
+	    // $verify->length = 4;        // 验证码位数
+	    // $verify->imageH = 34;       // 验证码高度
+	    // $verify->useImgBg = true;   // 开启验证码背景
+	    // $verify->useNoise = false;  // 关闭验证码干扰杂点
+	     $verify->entry();
 	}
 }
 
--- BBS/Home/Model/MessageModel.class.php
+++ BBS/Home/Model/MessageModel.class.php
@@ -2,28 +2,34 @@
 namespace Home\Model;
 use Think\Model;
 class MessageModel extends Model{
-	//type为sms时走这里
+	//type为sms时走这里,与发信箱和收信箱有关的。
 	public function sms(){
+		//设定好每页显示5条。
 		$row=5;
 		$users=D("users");
+		//只搜用户类的id和昵称nickname，用来设置一个专门的id和nickname键值对数组。
 		$data=$users->field('id,nickname')->select();
 		$userslist=[];
+		//将id作为键，昵称作为值。
 		foreach($data as $key=>$val){
 			$userslist[$val['id']]=$val['nickname'];
 		}
 		//参数action不同时走不同的条件。
-		//参数action为send时。
+		//参数action为send时。就是进入了发件箱
 		if($_GET['action']=="send"){
-			
+			//找出所有该用户发出的所有短消息。
 			$map['senderid']=['eq',1];
+			//得到总条数
 			$totalRow=$this->where($map)->count();
 			$page=new \Think\Page($totalRow,$row);
 			$list=$this->where($map)->limit($page->firstRow.",".$page->listRows)->select();
+			//将总条数赋值给$count，用来显示页面显示总条数的。
 			$count=$totalRow;
+			//$list数组新增一个键值receivername，用来放置对方的昵称。
 			foreach($list as $key=>&$val){
 				$val['receivername']=$userslist[$val['receiverid']];
 			}
-		}elseif($_GET['action']=="receive"){//参数action为receive时。
+		}elseif($_GET['action']=="receive"){//参数action为receive时。是收件箱，具体和send一样。
 			$map['receiverid']=['eq',1];
 			$totalRow=$this->where($map)->count();
 			$page=new \Think\Page($totalRow,$row);
@@ -32,7 +38,7 @@ class MessageModel extends Model{
 			foreach($list as $key=>&$val){
 				$val['sendername']=$userslist[$val['senderid']];
 			}
-		}elseif(empty($_GET)){//完全没有type和action参数时。
+		}elseif(empty($_GET)){//完全没有type和action参数时。是收件箱，具体和send一样。
 			$map['receiverid']=['eq',1];
 			$totalRow=$this->where($map)->count();
 			$page=new \Think\Page($totalRow,$row);
@@ -41,7 +47,7 @@ class MessageModel extends Model{
 			foreach($list as $key=>&$val){
 				$val['sendername']=$userslist[$val['senderid']];
 			}
-		}elseif(I('get.type')=='sms' && count(I('get.'))==1){//只有sms参数没有action参数时。
+		}elseif(I('get.type')=='sms' && count(I('get.'))==1){//只有sms参数没有action参数时。是收件箱。具体和send一样。
 			$map['receiverid']=['eq',1];
 			$totalRow=$this->where($map)->count();
 			$page=new \Think\Page($totalRow,$row);
@@ -51,31 +57,35 @@ class MessageModel extends Model{
 				$val['sendername']=$userslist[$val['senderid']];
 			}
 		}elseif(I('get.action')=='info'){//短信息具体页面。
-			//用户点击了收件箱时，立刻将状态改为2已读
+			//用户点击了某条短消息时，立刻将该短消息状态改为2已读，0为未读，1为已读，3为删除。
 			$titleid=I('get.titleid');
+			//get.status是前端页面传过来的，status==1是用户点击了
 			if(I('get.status')==1){
 				$where['id']=['eq',$titleid];
-				$data['status']=2;
+				$data['status']=1;
 				$this->where($where)->save($data);
 				
 			}
-			
+			//找到要点击进去的这条短消息
 			$arr=$this->find($titleid);
+			//找到该用户发的所有信息。
 			if($arr['senderid']==1){
-				$arr['senderid']=['eq',1];
-				$count=$this->where($arr)->count();
+				$arrlist['senderid']=['eq',1];
+				$count=$this->where($arrlist)->count();
 				$map['senderid']=['eq',1];
 				$subjectlist=$this->field('id')->where($map)->select();
 								
 			}else{
-				$arr['receiverid']=['eq',1];
-				$count=$this->where($arr)->count();
+				//不是发的消息，就是收的消息。
+				//查询出该用户收到的所有信息。
+				$arrlist['receiverid']=['eq',1];
+				$count=$this->where($arrlist)->count();
 				$map['receiverid']=['eq',1];
 				$subjectlist=$this->field('id')->where($map)->select();
 				
 				
-
 			}
+
 			//用$i间接得到$a,$a的作用是遍历了几次得到了$titleid的值，这样就可以知道它的前后id，就可以方便显示上一条和后一条了。
 			$i=0;
 			//$subjectlist是个二维数组，下标从0开始递增的。
@@ -91,44 +101,57 @@ class MessageModel extends Model{
 				$nextid=$a+1;
 				$arr['lastid']=0;
 				$arr['nextid']=$subjectlist[$nextid]['id'];
-			}elseif($a==count($subjectlist)-1 && count($subjectlist)>1){
+			}elseif($a==count($subjectlist)-1 && count($subjectlist)>1){//条数大于1的，刚刚好$a又是最后一条，那么就没有下一条了。
 				$lastid=$a-1;
 				$arr['lastid']=$subjectlist[$lastid]['id'];					
 				$arr['nextid']=0;
-			}elseif(count($subjectlist)==1){
+			}elseif(count($subjectlist)==1){//条数刚好等于1的，没有上一条，也没有下一条。
 				$arr['lastid']=0;
 				$arr['nextid']=0;
-			}else{
+			}else{//有上一条，也有下一条
 				$nextid=$a+1;
 				$lastid=$a-1;
 				$arr['lastid']=$subjectlist[$lastid]['id'];
 				$arr['nextid']=$subjectlist[$nextid]['id'];
 			}
 			$users=D('users');
+			//从用户是发信人还是收信人找出对方的昵称。
 			if($arr['senderid']==1){
-				//由发送人id找出这个人的昵称。
+				//由收信人id找出这个人的昵称。
 				$userslist=$users->find($arr['receiverid']);
-				//找出昵称。
+				//找出对方昵称。
 				$arr['sendername']=$userslist['nickname'];
-				$arr['count']=count($subjectlist);
 			}else{
-				//由发送人id找出这个人的昵称。
+				//由发信人id找出这个人的昵称。
 				$userslist=$users->find($arr['senderid']);
-				//找出昵称。
+				//找出对方昵称。
 				$arr['sendername']=$userslist['nickname'];
-				$arr['count']=count($subjectlist);
 			}
 
-			//统一将$list作为二维数组传过去显示。
+			
+			//将$list作为二维数组传过去显示。
 			$list=[];
 			$list[]=$arr;
+		}elseif(I('get.type')=='notice'){
+			dump(11111);
+		}elseif(I('get.type')=='request'){
+			dump(11111);
 		}else{//其它情况就是乱来的。
 			$this->error("没有你要找的网页");
 		}
-		return [
-			'list'=>$list,
-			'count'=>$count,
-		];
+		if($page){
+			return [
+				'list'=>$list,
+				'count'=>$count,
+				'show'=>$page->show(),
+			];
+		}else{
+			return [
+				'list'=>$list,
+				'count'=>$count,
+			];
+		}
+		
 	}
 
 	//处理删除。
@@ -140,19 +163,38 @@ class MessageModel extends Model{
 		//返回删除结果。
 		return $res;
 	}
-
+	//标记已读处理
 	public function pro_tag(){
 		$arr=explode(",",I('get.data'));
 		$map['id']=['in',$arr];
 		$data['status']=1;
 		$res=$this->where($map)->save($data);
-		//返回删除结果。
+		//返回处理结果
 		return $res;
 	}
-
+	//回复内容，是回复，不是发新消息。
 	public function pro_add(){
 		$data=I("post.");
 		$res=$this->add($data);
 		return $res;
+	}
+
+	public function pro_post(){
+		$users=D('users');
+		$map['nickname']=['eq',I('post.receivername')];
+		$res=$users->where($map)->select();
+		if(!$res){
+			return 0;
+		}
+		$add['receiverid']=2;
+		$add['senderid']=1;
+		$add['title']=['eq',I('post.title')];
+		$add['content']=['eq',I('post.content')];
+		$res=$this->add($add);
+		if($res){
+			return 1;
+		}else{
+			return 2;
+		}
 	}
 }
