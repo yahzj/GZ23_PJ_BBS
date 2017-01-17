c58c1e98db96da4f6510963d580372ac1d207d10
dahuang <332685057@qq.com>
2017-01-17 13:47:59 +0800

    1.17上午更新

--- BBS/Home/Controller/SectionsController.class.php
+++ BBS/Home/Controller/SectionsController.class.php
@@ -4,8 +4,34 @@ use Think\Controller;
 class SectionsController extends EmptyController {
     public function index(){
     	$s_id=(int)I('get.s');
+    	// 获取数据库中本板块的相关信息
+    	$section=D('Admin/sections');
+    	$Smap['id']=$s_id;
+    	$Sdata=$section->field('name,path,top')->where($Smap)->select();
+    	$data['SectionTop']=$Sdata['0']['top'];
+    	// 拼接成导航路径
+    	$Sdata['0']['path'].=$s_id;
+    	$path=explode( ',',$Sdata['0']['path']);
+    	foreach ($path as $k=>$v) {
+    		if ($v==='0') {
+    			$data['link'][$k]['pname']='首页';
+    			$data['link'][$k]['path']=U('index/index');
+    		}else{
+    			$data['link'][$k]['pname']=$Sdata['0']['name'];
+    			$data['link'][$k]['path']=U("sections/index","s=$v");
+    		}
+    	}
+
+    	// 获取数据库中本版块的子板块
+    	$Ssmap['parent_id']=$s_id;
+    	$Ssdata=$section->field('name,id,introduce')->where($Ssmap)->select();
+    	$data['child']=$Ssdata;
+    	foreach ($data['child'] as $key => &$val) {
+    		$val['link']=U('sections/index',"s=".$val['id']);
+    	}
+    	// 获取库中主题相关信息
+	    $obj=D('Admin/subject');
     	$map['section_id']=$s_id;
-	    $obj=D('admin/subject');
 
 	    $rows=25;
 	    //统计总数量
@@ -13,13 +39,8 @@ class SectionsController extends EmptyController {
 	    // 实例化分页类
 		$page = new \Think\Page( $totalRow,$rows);
 
-	    //板块查询需要的SQL语句
-		// $sql = 'select su.name as name,su.uid as uid,su.followtime as f_time,su.fid as fid,su.floor as floor,su.addtime as a_time,se.id,su.section_id from mybbs_subject su,mybbs_sections se  where se.id=su.section_id and se.id='.$s_id;
-		// .$map.' order by a.id '.$sort.' limitl '.$p->firstRow.','.$p->listRows;
-
 		//TP语句拼装
 		$data['list'] =$obj->field('name,uid,followtime,fid,floor,addtime')->where($map)->order('`followtime` desc')->limit( $page->firstRow . ',' . $page->listRows   )->select();
-
 		// $data['list'] =$obj->field('mybbs_subject.name as name,mybbs_subject.uid as uid,mybbs_subject.followtime as f_time,mybbs_subject.fid as fid,mybbs_subject.floor as floor,mybbs_subject.addtime as a_time,mybbs_sections.id,mybbs_subject.section_id')->join('mybbs_sections on mybbs_subject.section_id=mybbs_sections.id')->where($map)->select();
 		// $data['list'] = $obj->query($sql);
 		if (empty($data['list'])) {
@@ -31,11 +52,11 @@ class SectionsController extends EmptyController {
 			$userid[]=$val[fid];
 		}
 		// dump($sql);
-		// dump($data['list']);
+		// dump($data);
 		// 进行查询
 		$user=D('admin/users');
-		$map[id]=array('in',$userid);
-		$userlist=$user->field('id,nickname')->where($map)->select();
+		$Umap[id]=array('in',$userid);
+		$userlist=$user->field('id,nickname')->where($Umap)->select();
 		$user=[];
 		foreach ($userlist as $key => $val) {
 			$user[$val['id']]=$val['nickname'];
